8fac9f20f17f97c4affdd5c7ef2b4a5c787bad30
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-07-19 13:54:12 +0900

    [Silver III] Title: 주유소, Time: 96 ms, Memory: 3588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -6,25 +6,24 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int>distance(n-1);
-    vector<int>price(n);
-    vector<int>dp(n-1);
+    vector<long long>distance(n-1);
+    vector<long long>price(n);
     for(int i=0;i<n-1;i++){
         cin>>distance[i];
     }
     for(int i=0;i<n;i++){
         cin>>price[i];
     }
-    dp[0] = distance[0] * price[0];
-    int minprice = price[0];
-    dp[1] = min(price[0]*(distance[0]+distance[1]), dp[0]+(distance[1]*price[1]));
-    minprice = minprice<price[1] ? minprice : price[1]; 
-    for(int i=2;i<n-1;i++){
-         minprice = minprice<price[i] ? minprice : price[i]; 
-         dp[i] = dp[i-1]+minprice*distance[i];
+    long long answer =  distance[0] * price[0];
+    long long minprice = price[0];
+    for(int i=1;i<n-1;i++){
+         if(minprice>price[i]){
+            minprice = price[i];
+         }
+         answer = answer +minprice*distance[i];
     }
 
-    cout<<dp[n-2];
+    cout<<answer;
     
 
 
