51c1da22abb292bc412428e3e675d132d46da8f7
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-07-19 13:42:34 +0900

    [Silver III] Title: 주유소, Time: 56 ms, Memory: 3196 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -16,9 +16,12 @@ int main(){
         cin>>price[i];
     }
     dp[0] = distance[0] * price[0];
+    int minprice = price[0];
     dp[1] = min(price[0]*(distance[0]+distance[1]), dp[0]+(distance[1]*price[1]));
+    minprice = minprice<price[1] ? minprice : price[1]; 
     for(int i=2;i<n-1;i++){
-        dp[i] = min(dp[i-1]+(distance[i]*price[i]), dp[i-2]+price[i-1]*(distance[i]+distance[i-1]));
+         minprice = minprice<price[i] ? minprice : price[i]; 
+         dp[i] = dp[i-1]+minprice*distance[i];
     }
 
     cout<<dp[n-2];
