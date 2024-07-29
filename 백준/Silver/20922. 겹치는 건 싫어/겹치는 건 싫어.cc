ccdd8c75591ee35a93de058ec5a4dc9ab1a67100
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-07-29 14:11:48 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 76 ms, Memory: 3460 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -6,25 +6,29 @@
 #include<unordered_map>
 using namespace std;
 
+long long inputa[100001];
 
 int main(){
     int a,b;
-    int start=0, length=0;
+    int length=0;
+    int temp=0,start=0,end=0;
+    memset(inputa, 0, sizeof(inputa));
     cin>>a>>b;
     int original[a];
     for(int i=0;i<a;i++){
-        cin>>original[i]; 
+        int qw;
+        cin>>qw;
+        original[i] = qw;
     }
-    unordered_map<int, int>answer;
-
-    for(int i=0;i<a;i++){
-          answer[original[i]]++;
-          while(answer[original[i]]>b){
-            answer[original[start]]--;
+    for(end=0;end<a;end++){
+        inputa[original[end]]++;
+        while(inputa[original[end]]>b){
+            inputa[original[start]]--;
             start++;
-          }
-          length = max(length, i-start+1);
+        }
+        length = max(length, end-start+1);
     }
+
     cout<<length;
 
 }
