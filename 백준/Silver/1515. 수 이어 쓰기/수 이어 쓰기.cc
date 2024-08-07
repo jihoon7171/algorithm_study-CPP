34b14cf8bd8aa1ffb99d46e1dbb20c0aa64018b8
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-08-07 15:40:11 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
@@ -1,27 +1,25 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
+
+
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    string a;
+    cin >> a;
+    int num=0, ptr=0;
+    while(num++<30001){
+        string temp = to_string(num);
+        for(int i=0;i<temp.size();i++){
+            if(temp[i] == a[ptr]){
+                ptr++;
+            }
+            if(ptr == a.size()){
+                cout<<num;
+                return 0;
+            }
+        }
 
-	string str = "";
-	cin >> str;
+    }
 
-	int num = 0, ptr = 0;
-
-	while(num++ < 30000) {
-		string num2str = to_string(num);
-		for(int i = 0; i < num2str.length(); i++) {
-			
-			if(str[ptr] == num2str[i]) {
-				ptr++;
-			}
-			if(ptr == str.length()) {
-				cout << num;
-				return 0;
-			}
-		}
-	}
-	return 0;
 }
