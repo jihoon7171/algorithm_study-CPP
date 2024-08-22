d8391cbbb385b5a5d1d1622cfb05aa7c04db861f
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-08-22 11:50:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<cmath>
+using namespace std;
+int dp[100001];
+int a,b;
+
+void dpsolution(int start){
+
+    for(int i=start+1;i<=b;i++){
+        if(i%2==0){
+            dp[i] = min(dp[i/2]+1, dp[i-1]+1);
+        }else{
+            dp[i] = min(dp[(i+1)/2]+2, dp[i-1]+1);
+        }
+    }
+}
+
+int main(){
+
+    cin>>a>>b;
+    dp[a] = 0;
+    for(int i=a;i>=0;i--){
+        dp[i] = a-i;
+    }
+    dpsolution(a);
+    cout<<dp[b];
+
+    
+}
