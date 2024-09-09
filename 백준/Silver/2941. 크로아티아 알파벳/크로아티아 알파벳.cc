1ef670041bc7a4f1b4951212772c9c24266c7c39
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-09-09 13:50:25 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int checkst(string as) {
+    int count = 0;
+    size_t pos = 0;
+
+    while (pos < as.size()) {
+        if (as.find("dz=", pos) == pos) {
+            count++;
+            pos += 3;
+        } else if (as.find("c=", pos) == pos) {
+            count++;
+            pos += 2;
+        } else if (as.find("c-", pos) == pos) {
+            count++;
+            pos += 2;
+        } else if (as.find("d-", pos) == pos) {
+            count++;
+            pos += 2;
+        } else if (as.find("lj", pos) == pos) {
+            count++;
+            pos += 2;
+        } else if (as.find("nj", pos) == pos) {
+            count++;
+            pos += 2;
+        } else if (as.find("s=", pos) == pos) {
+            count++;
+            pos += 2;
+        } else if (as.find("z=", pos) == pos) {
+            count++;
+            pos += 2;
+        } else {
+            count++;
+            pos++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    string as;
+    cin >> as;
+    cout << checkst(as) << endl;
+    return 0;
+}
