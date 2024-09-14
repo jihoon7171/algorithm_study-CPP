3b381d4009afe2c9fc431c4142fd79f3556e763c
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-09-14 15:09:35 +0900

    [Silver I] Title: 트럭, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+#include<unordered_set>
+#include<queue>
+using namespace std;
+ 
+
+int main() {
+    int n,w,l,now=0,count=0;
+    cin>>n>>w>>l;
+    queue<int>truck;
+    queue<pair<int, int>>bridge;
+    for(int i=0;i<n;i++){
+       int a;
+       cin>>a;
+       truck.push(a);
+    }
+    while(!truck.empty() || !bridge.empty()){
+        if(!bridge.empty()&&bridge.front().second>=w){
+            now -= bridge.front().first;
+            bridge.pop();
+        }
+
+        if(!truck.empty() &&now + truck.front() <= l){
+            now +=truck.front();
+            bridge.push({truck.front(), 0});
+            truck.pop();
+        }
+        int size = bridge.size();
+        for(int i=0;i<size;i++){
+            pair<int, int>current = bridge.front();
+            current.second++;
+            bridge.pop();
+            bridge.push(current);
+        }
+
+
+        count++;
+    }
+    cout<<count;
+}
