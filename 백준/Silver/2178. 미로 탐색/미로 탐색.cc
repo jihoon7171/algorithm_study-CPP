3a513e180e10d7761a8e0c3de803cce2147c6f5b
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-09-16 13:45:07 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,55 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include<algorithm>
-#include<map>
+#include<unordered_set>
 #include<queue>
 using namespace std;
-int a,b;
-int dx[4] = {0,0,1,-1};
-int dy[4]={1,-1,0,0};
-int maze[100][100];
-int visited[100][100];
-int dist[100][100];
-
-
-void bfs(int startx, int starty){
+int n,m;
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+void bfs(int x, int y, vector<vector<int>>&maze){
+    vector<vector<int>>dist(n,vector<int>(m,-1));
+    dist[y][x] = 1;
     queue<pair<int, int>>q;
-    visited[startx][starty] = 1;
-    dist[startx][starty] = 1;
-    q.push(make_pair(startx,starty));
+    q.push({y,x});
     while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
+        int nowy = q.front().first;
+        int nowx = q.front().second;
         q.pop();
         for(int i=0;i<4;i++){
-            int dxx = x+dx[i];
-            int dyy = y+dy[i];
-            if((0<=dxx && dxx<a) &&(0<=dyy && dyy<b) && maze[dxx][dyy] == 1 && visited[dxx][dyy] == 0 ){
-                visited[dxx][dyy] = 1;
-                q.push(make_pair(dxx,dyy));
-                dist[dxx][dyy] = dist[x][y]+1;
-
-
+            int newx = nowx+dx[i];
+            int newy = nowy+dy[i];
+            if(newx>=0&&newx<m&&newy>=0&&newy<n&&dist[newy][newx]==-1&&maze[newy][newx]==1){
+                dist[newy][newx] = dist[nowy][nowx]+1;
+                q.push({newy, newx});
             }
-
         }
     }
+    cout<<dist[n-1][m-1];
+
 
 }
+ 
 
-
-int main(){
-    cin>>a>>b;
-    for(int i=0;i<a;i++){
-        string as;
-        cin>>as;
-        for(int j=0;j<b;j++){
-            maze[i][j] = as[j] - '0';
+int main() {
+    int count=0;
+    cin>>n>>m;
+    vector<vector<int>>maze(n,vector<int>(m));
+    for(int i=0;i<n;i++){
+        string temp;
+        cin>>temp;
+        for(int j=0;j<temp.size();j++){
+            maze[i][j] = temp[j]-'0';
         }
     }
-    bfs(0,0);
-    cout<<dist[a-1][b-1];
-
-    
+    bfs(0,0,maze);
 }
