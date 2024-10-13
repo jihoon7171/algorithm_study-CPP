11f3cfa1d98989cbec2fc42b9e85a7b821f3154b
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2024-10-13 23:14:19 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+pair<int, int> godirection[4] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> clean(n, vector<int>(m));
+    pair<int, int> now;
+    cin >> now.first >> now.second;
+    int startdirection;
+    cin >> startdirection;
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> clean[i][j];
+        }
+    }
+
+    int nowdirection;
+    if (startdirection == 1) {
+        nowdirection = 3; 
+    } else if (startdirection == 3) {
+        nowdirection = 1; 
+    } else {
+        nowdirection = startdirection;
+    }
+
+    while (true) {
+        bool checking = false;
+        
+
+        if (clean[now.first][now.second] == 0) {
+            clean[now.first][now.second] = 2;
+        }
+
+        int temp = nowdirection;
+        
+      
+        for (int i = 0; i < 4; i++) {
+            temp = (temp + 1) % 4;  
+            int goy = now.first + godirection[temp].first;
+            int gox = now.second + godirection[temp].second;
+            if (goy >= 0 && goy < n && gox >= 0 && gox < m && clean[goy][gox] == 0) {
+                now = {goy, gox};
+                nowdirection = temp;
+                checking = true;
+                break;
+            }
+        }
+
+        if (checking) continue;
+        int backy = now.first - godirection[nowdirection].first;
+        int backx = now.second - godirection[nowdirection].second;
+        if (backy >= 0 && backy < n && backx >= 0 && backx < m && clean[backy][backx] != 1) {
+            now = {backy, backx};
+        } else {
+            break;
+        }
+    }
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (clean[i][j] == 2) {
+                count++;
+            }
+        }
+    }
+
+    cout << count << "\n";
+    return 0;
+}
