4f4c13bdae5d57b9777ef20d89f7e0ecd250ae79
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2025-01-01 16:35:43 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -1,66 +1,68 @@
-#include <iostream>
+ #include <iostream>
 #include <vector>
+#include <climits>
 #include <string>
-#include <set>
 #include <algorithm>
-
+#include<stack>
+#include<set>
+#include<map>
+#include<queue>
+#include<limits.h>
+#include<queue>
 using namespace std;
+int a,b;
+vector<char>pass;
+set<string>answer;
 
-int a, b;
-vector<char> pass;
-set<string> answer;
-
-void dfs(int count, vector<char>& ans, int index) {
-    if (count == a) {
-        int mcount = 0; // 모음 개수
-        int jcount = 0; // 자음 개수
-
-        for (int i = 0; i < a; i++) {
-            if (ans[i] == 'i' || ans[i] == 'o' || ans[i] == 'a' || ans[i] == 'u' || ans[i] == 'e') {
+void dfs(int count, vector<char>ans, int index){
+    if(count == a){
+        int mcount=0;
+        int jcount =0;
+        for(int i=0;i<a;i++){
+            if(ans[i] == 'i' || ans[i] == 'o'||ans[i] == 'a'||ans[i]=='u'||ans[i] == 'e'){
                 mcount++;
-            } else {
+            }else{
                 jcount++;
             }
         }
-
         bool checking = true;
-        for (int i = 0; i < a - 1; i++) {
-            if (ans[i] > ans[i + 1]) {
-                checking = false;
-                break;
+            for(int i=0;i<a-1;i++){
+                if(ans[i] - 'a' > ans[i+1]-'a'){
+                    checking = false;
+                    break;
+                }
             }
-        }
-
-        if (mcount >= 1 && jcount >= 2 && checking) {
-            string temp(ans.begin(), ans.end());
-            answer.insert(temp);
-        }
-        return;
+            string temp;
+            if(mcount>=1 && jcount>=2&&checking){
+                for(int i=0;i<a;i++){
+                   cout<<ans[i];
+                }
+                cout<<"\n";
+            }
     }
+    for(int i = index;i<b;i++){
+            ans[count] = pass[i];
+            dfs(count+1, ans, i+1);
 
-    for (int i = index; i < b; i++) {
-        ans[count] = pass[i];
-        dfs(count + 1, ans, i + 1);
     }
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> a >> b;
+    cin>>a>>b;
     pass.resize(b);
-    for (int i = 0; i < b; i++) {
-        cin >> pass[i];
+    vector<bool>used(b,false);
+    for(int i=0;i<b;i++){
+        cin>>pass[i];
     }
-
-    vector<char> ans(a);
+    vector<char>ans(a);
     sort(pass.begin(), pass.end());
-    dfs(0, ans, 0);
+    dfs(0,ans,0);
 
-    for (const string& temp : answer) {
-        cout << temp << "\n";
+    for( string temp : answer){
+        cout<<temp<<"\n";
     }
+    
 
-    return 0;
 }
