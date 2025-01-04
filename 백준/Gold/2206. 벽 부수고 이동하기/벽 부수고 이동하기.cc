3cecacb14183646a6a31845e7644799c87b23d41
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2025-01-04 16:21:36 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 88 ms, Memory: 15560 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,61 +1,66 @@
 #include <iostream>
 #include <vector>
 #include <climits>
-#include<queue>
 #include <string>
 #include <algorithm>
+#include<stack>
+#include<set>
+#include<map>
+#include<queue>
+#include<limits.h>
+#include<queue>
 using namespace std;
+    int n,m;
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
-int map[1000][1000];
-int visited[1000][1000][2];
-int n,m;
+int answer = INT_MAX;
+queue<pair<int, int>>wall;
+int lab[1001][1001];
+int dis[1001][1001][2];
 
 int bfs(){
     queue<pair<pair<int, int>, int>>q;
-    q.push({{0,0},0});
-    visited[0][0][0] = 1;
+    dis[1][1][0] = 1;
+    q.push({{1,1},0});
     while(!q.empty()){
-        int nowx = q.front().first.first;
-        int nowy = q.front().first.second;
-        int broke = q.front().second;   
-        if(nowx == m-1 && nowy == n-1){
-            return visited[nowy][nowx][broke];
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int broke = q.front().second;
+        if(x == m && y == n){
+            return dis[y][x][broke];
         }
-
         q.pop();
         for(int i=0;i<4;i++){
-            int newx = nowx+dx[i];
-            int newy = nowy+dy[i];
-            if(newx>=0&&newx<m&&newy>=0&&newy<n){
-                if(map[newy][newx] == 0 && visited[newy][newx][broke] == 0){
-                    visited[newy][newx][broke] = visited[nowy][nowx][broke]+1;
-                    q.push({{newx, newy}, broke});
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx>=1&&nx<=m&&ny>=1&&ny<=n){
+                if(lab[ny][nx] == 1 && broke == 0){
+                    dis[ny][nx][broke+1] = dis[y][x][broke]+1;
+                    q.push({{nx,ny},broke+1});
                 }
-                if(map[newy][newx] == 1 && broke == 0){
-                    visited[newy][newx][broke+1] = visited[nowy][nowx][broke]+1;
-                    q.push({{newx, newy}, broke+1});
+                if(lab[ny][nx] == 0 && dis[ny][nx][broke] == 0){
+                    dis[ny][nx][broke] = dis[y][x][broke]+1;
+                    q.push({{nx,ny},broke});
                 }
-
             }
         }
     }
     return -1;
-
 }
-
-
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     cin>>n>>m;
-    for(int i=0;i<n;i++){
+    for(int i=1;i<=n;i++){
         string temp;
         cin>>temp;
-        for(int j=0;j<m;j++){
-            map[i][j] = temp[j]-'0';
+        for(int j=0;j<temp.size();j++){
+            lab[i][j+1] = temp[j] - '0';
         }
     }
     cout<<bfs();
+
+
+
     
-
-
 }
