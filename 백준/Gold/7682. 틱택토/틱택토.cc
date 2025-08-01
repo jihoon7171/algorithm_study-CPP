7cf5057cb5fd137500201fd354a24de0d327251d
채지훈 <73058104+jihoon7171@users.noreply.github.com>
2025-08-01 19:46:51 +0900

    [Gold V] Title: 틱택토, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/7682. 틱택토/틱택토.cc
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<climits>
+#include<tuple>
+#include<string>
+using namespace std;
+
+// 가로 세로 대각선 방향으로 3칸 이상 -> 가능할 수 없는 게임
+// 게임판이 가득 
+
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+
+//pair<pair<int, int>, int> ch[8] = {{{2,4},6},{{0,4},8},{{0,1},2},{{3,4},5},{{6,7},8},{{0,3},6},{{1,4},7},{{2,5},8}};
+tuple<int, int, int> ch[8] = {
+    {2, 4, 6},
+    {0, 4, 8},
+    {0, 1, 2},
+    {3, 4, 5},
+    {6, 7, 8},
+    {0, 3, 6},
+    {1, 4, 7},
+    {2, 5, 8}
+};
+
+void iswin(string temp, int dotcount, int xcount, int ocount){
+    bool xwin = false;
+    bool owin = false;
+    for(int i = 0; i < 8; i++){
+        int a, b, c;
+        tie(a, b, c) = ch[i];
+        if(temp[a] == temp[b] && temp[b] == temp[c]){
+            if(temp[a] == 'X') xwin = true;
+            if(temp[a] == 'O') owin = true;
+        }
+    }
+
+    // 둘 다 이긴 상태면 무조건 invalid
+    if(xwin && owin){
+        cout << "invalid\n";
+        return;
+    }
+
+    // X가 이겼다면 X는 O보다 1개 더 많아야 함
+    if(xwin){
+        if(xcount == ocount + 1){
+            cout << "valid\n";
+            return;
+        }
+        cout << "invalid\n";
+        return;
+    }
+
+    // O가 이겼다면 X와 O 개수 같아야 함
+    if(owin){
+        if(xcount == ocount){
+            cout << "valid\n";
+            return;
+        }
+        cout << "invalid\n";
+        return;
+    }
+
+    // 승자가 없고, 판이 다 찼다면 무승부(valid)
+    if(dotcount == 0){
+        cout << "valid\n";
+        return;
+    }
+
+    // 이 외에는 진행중인데 게임 끝난 상태 -> invalid
+    cout << "invalid\n";
+}
+
+void checking(string temp){
+    int xcount = 0;
+    int ocount = 0;
+    int dotcount = 0;
+    for(int i=0;i<9;i++){
+        if(temp[i] == 'X'){
+            xcount++;
+        }else if(temp[i] == 'O'){
+            ocount++;
+        }else{
+            dotcount++;
+        }
+    }
+    //cout<<xcount<<" "<<ocount<<" "<<dotcount<<"\n";
+    //꽉 찼을경우
+    if(dotcount == 0){
+        if(xcount != ocount+1){
+            cout<<"invalid\n";
+            return ;
+        }else{
+            iswin(temp, dotcount, xcount, ocount);
+        }
+    }else{
+        // if(xcount  > ocount+2 || ocount > xcount+2 ){
+        //     cout<<"invalid\n";
+        //     return ;
+        // }
+        if(dotcount % 2 == 0){
+            if(xcount != ocount+1){
+                cout<<"invalid\n";
+                return ;
+            }
+        }else{
+            if(xcount != ocount){
+                cout<<"invalid\n";
+                return ;
+            }
+        }
+        iswin(temp, dotcount, xcount, ocount);
+    }
+
+}
+
+
+int main(){
+    while(1){
+        string temp;
+        cin>>temp;
+        if(temp == "end") break;
+        checking(temp);
+
+    }
+}
